15a5aba59a4a77360dc0b434eac0f6ce5103431f
Abdul Malek <abmalek586@gmail.com>
2025-08-10 01:09:09 +0600

    Intial file added

---
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,24 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+    
+    public String generateEmailReply(EmailRequest emailRequest){
+        // Build the pormt
+
+        String promt = buildPromt(emailRequest);
+        // Craft the promt
+        // Request and get response
+        // return the response
+
+        return null;
+    }
+
+    private String buildPromt(EmailRequest emailRequest) {
+        
+
+        return null;
+    }
+}
